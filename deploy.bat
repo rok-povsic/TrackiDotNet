e3d2da62abd4cfe8c0924279fb48398983006e91
Rok Povšič <rok.povsic@gmail.com>
2017-05-31 23:13:50 +0200

    Changed deploy location.

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /t:Rebuild /p:Configuration=Release Tracki.sln
-set folder="C:/Tracki"
+set folder="D:/Dropbox/Tracki"
 if not exist %folder% mkdir %folder%
 cp -R Tracki/bin/Release/* %folder%
