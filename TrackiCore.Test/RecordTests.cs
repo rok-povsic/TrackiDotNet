98b885afc4fdd75ad6b70583b0e96a4fda3593d7
Rok Povsic <rok.povsic@gmail.com>
2020-07-25 10:48:11 +0200

    Cleanup test comments

--- TrackiCore.Test/RecordTests.cs
+++ TrackiCore.Test/RecordTests.cs
@@ -9,17 +9,14 @@ namespace TrackiCore.Test
         [Fact]
         public void Test1()
         {
-            // Arrange.
             var timeTracking = new FkTimeTracking();
 
             var category = new WorkTask("Test");
             var record = new Record(category, timeTracking);
 
-            // Act.
             record.Start();
             timeTracking.SetStop(TimeSpan.FromMinutes(5));
 
-            // Assert.
             Assert.Equal(TimeSpan.FromMinutes(5), record.Stop());
         }
     }
